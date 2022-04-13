11219240ef8027d0800289a9f5be91fdfd5dc86b
Munde-J <mundeangelah@gmail.com>
2022-04-13 09:27:02 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var sentence= "Kenya is a beautiful country"
+    var words =sentence.split(" ")
+    println(words[0])
+    println(words[1])
+    println(words[2])
+    println(words[3])
+    println(words[4])
+//    println(sentence.replace("a","x"))
+//
+//    var empty=""
+//    var blank=" "
+//    println(empty.isEmpty())
+    }
